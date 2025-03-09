38bed4266ae92858f02a6c6c3ff9d96ab604b5f9
Phatchung <chungtanphat2000@gmail.com>
2025-03-09 11:17:39 +0700

    first-demo

---
+++ src/demo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class demo {
+    public static void main(String[] args) {
+        Scanner dc=new Scanner(System.in);
+        System.out.print("Nhap: ");
+        String a=dc.nextLine();
+        System.out.println("Ban vua nhap: "+a);
+
+
+    }
+}
